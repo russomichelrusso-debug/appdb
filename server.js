306b9850e9f8f75b810054544df7635b22a14bfa
russomichelrusso-debug <russomichelrusso@gmail.com>
2026-08-23 15:18:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
   if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
 });
